fd61a5451cf45e3eecb858bf741318d81f739d8b
Yuang Yao <yaoyuang1996@gmail.com>
2017-11-19 20:53:15 -0500

    fix

--- exp.py
+++ exp.py
@@ -1,26 +1,26 @@
 
 exp2={
-	'learning_rate' = 0.001,
-	'training_iters' = 50000,
-	'step_display' = 50,
-	'step_save' = 500,
-	'exp_name' = 'exp2',
-	'num' = '',
+	'learning_rate' : 0.001,
+	'training_iters' : 50000,
+	'step_display' : 50,
+	'step_save' : 500,
+	'exp_name' : 'exp2',
+	'num' : '',
 
-	'train' = True,
-	'validation' = True,
-	'selectedmodel'="VGG"
+	'train' : True,
+	'validation' : True,
+	'selectedmodel':"VGG"
 }
 
 expVal={
-	'learning_rate' = 0.001,
-	'training_iters' = 50000,
-	'step_display' = 50,
-	'step_save' = 500,
-	'exp_name' = 'exp2',
-	'num' = '500',
+	'learning_rate' : 0.001,
+	'training_iters' : 50000,
+	'step_display' : 50,
+	'step_save' : 500,
+	'exp_name' : 'exp2',
+	'num' : '500',
 
-	'train' = False,
-	'validation' = True,
-	'selectedmodel'="VGG"
+	'train' : False,
+	'validation' : True,
+	'selectedmodel':"VGG"
 }
