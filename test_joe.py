193fc81c0f6feee23bda59fcc796f96a2ea4fac2
joehuang <joehuang@mit.edu>
2017-11-20 17:49:07 -0500

    new data processor

---
+++ test_joe.py
@@ -0,0 +1,4 @@
+from data_processor import xml_extract
+object_path = '/Users/joehuang/Documents/MIT Classes/6.867/Project/SceneClassification/data/objects/train/a/abbey/00000001.xml'
+
+xml_extract(object_path, 'a')
