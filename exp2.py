c5654ae3790ef95730c133b5b4e10d938473e83e
joehuang <joehuang@mit.edu>
2017-12-10 04:23:50 -0500

    updae

--- exp2.py
+++ exp2.py
@@ -24,8 +24,25 @@ exp2={
 		'exp_name' : 'exp2',
 		'pretrainedStep' : 0,
 		'batch_size':16,
-		'lam': 0.5,
+		'lam': 0.1,
 		'joint_ratio':0.5,
+		'plot':True,
+
+		'train' : True,
+		'validation' : True,
+		'test': True,
+		'selectedmodel':"vgg_seg1"
+	},
+	'exp3':{
+		'learning_rate' : 1e-5,
+		'training_iters' : 10000,
+		'step_display' : 100,
+		'step_save' : 500,
+		'exp_name' : 'exp3',
+		'pretrainedStep' : 0,
+		'batch_size':16,
+		'lam': 0.1,
+		'joint_ratio':0.2,
 		'plot':True,
 
 		'train' : True,
