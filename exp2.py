977352ec82679a3c3955a181926a8b88188cfba5
joehuang <joehuang@mit.edu>
2017-12-10 02:25:52 -0500

    update.sh

--- exp2.py
+++ exp2.py
@@ -6,9 +6,26 @@ exp2={
 		'step_save' : 500,
 		'exp_name' : 'exp1',
 		'pretrainedStep' : 0,
-		'batch_size':40,
+		'batch_size':16,
 		'lam': 0.5,
 		'joint_ratio':1,
+		'plot':True,
+
+		'train' : True,
+		'validation' : True,
+		'test': True,
+		'selectedmodel':"vgg_seg1"
+	},
+	'exp2':{
+		'learning_rate' : 1e-5,
+		'training_iters' : 10000,
+		'step_display' : 100,
+		'step_save' : 500,
+		'exp_name' : 'exp2',
+		'pretrainedStep' : 0,
+		'batch_size':16,
+		'lam': 0.5,
+		'joint_ratio':0.5,
 		'plot':True,
 
 		'train' : True,
