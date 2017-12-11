dd83b0af2581c666ab0d85a29d0d813ccad945ec
joehuang <joehuang@mit.edu>
2017-12-10 22:07:14 -0500

    joe_debug

--- exp_joe.py
+++ exp_joe.py
@@ -2,7 +2,7 @@ exp_joe={
 	'debug1':{
 		'learning_rate' : 1e-5,
 		'training_iters' : 10000,
-		'step_display' : 100,
+		'step_display' : 20,
 		'step_save' : 500,
 		'exp_name' : 'debug1',
 		'pretrainedStep' : 0,
