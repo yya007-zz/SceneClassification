d2e7cfb237005c19fa15754cdecbbbe5268ba6e1
joehuang <joehuang@mit.edu>
2017-12-10 23:37:08 -0500

    update

--- architect_joe.py
+++ architect_joe.py
@@ -5,7 +5,7 @@ import CNNModels_joe
 
 class vgg_seg1:
     def __init__(self, x, seg_labels, keep_dropout, train_phase):
-        self.logits_seg = CNNModels_joe.VGG_Seg1(x, keep_dropout, train_phase, num_seg_classes=176, batch_norm=True, seg=True, random_init_seg_score_fr=True, debug=True)
+        self.logits_seg = CNNModels_joe.VGG_Seg1(x, keep_dropout, train_phase, num_seg_classes=176, batch_norm=True, seg=True, random_init_seg_score_fr=False, debug=True)
         #self.loss = loss_seg(seg_labels, self.logits_seg)
         self.loss = loss_seg_tanh(seg_labels, self.logits_seg)
 
