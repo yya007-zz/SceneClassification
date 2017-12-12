2e6667b3726f77c5501440dcd49bb5129d485407
joehuang <joehuang@mit.edu>
2017-12-12 09:53:27 -0500

    update

--- exp_joe.py
+++ exp_joe.py
@@ -1,14 +1,14 @@
 exp_joe={
 	'exp1':{
 		'learning_rate_class' : 1e-5,
-		'learning_rate_seg' : 1e-10,
+		'learning_rate_seg' : 1e-6,
 		'training_iters' : 10000,
 		'step_display' : 50,
 		'step_save' : 500,
 		'exp_name' : 'exp1',
 		'pretrainedStep' : 0,
 		'batch_size':14,
-		'joint_ratio':0.5,
+		'joint_ratio':0.05,
 		'plot':True,
 
 		'train' : True,
@@ -25,7 +25,7 @@ exp_joe={
 		'exp_name' : 'exp2',
 		'pretrainedStep' : 0,
 		'batch_size':14,
-		'joint_ratio':0.5,
+		'joint_ratio':0.1,
 		'plot':True,
 
 		'train' : True,
