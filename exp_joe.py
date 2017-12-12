aae96d10e943bed0138e7a4f0f82bfc3cfc66873
joehuang <joehuang@mit.edu>
2017-12-12 16:31:07 -0500

    update

--- exp_joe.py
+++ exp_joe.py
@@ -19,7 +19,7 @@ exp_joe={
 	'exp2':{
 		'learning_rate_class' : 1e-4,
 		'learning_rate_seg' : 1e-5,
-		'training_iters' : 10000,
+		'training_iters' : 20000,
 		'step_display' : 50,
 		'step_save' : 500,
 		'exp_name' : 'exp2',
@@ -34,15 +34,15 @@ exp_joe={
 		'selectedmodel':"vgg_seg1_mask"
 	},
 	'exp3':{
-		'learning_rate_class' : 1e-5,
-		'learning_rate_seg' : 1e-6,
-		'training_iters' : 10000,
+		'learning_rate_class' : 1e-4,
+		'learning_rate_seg' : 1e-5,
+		'training_iters' : 20000,
 		'step_display' : 50,
 		'step_save' : 500,
 		'exp_name' : 'exp3',
 		'pretrainedStep' : 0,
 		'batch_size':14,
-		'joint_ratio':0.05,
+		'joint_ratio':0.1,
 		'plot':True,
 
 		'train' : True,
