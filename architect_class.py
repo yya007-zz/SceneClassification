5bd96c27884e13545a476dbf6bb838e73bafd242
joehuang <joehuang@mit.edu>
2017-12-12 03:03:30 -0500

    update

---
+++ architect_class.py
@@ -0,0 +1,22 @@
+import os, datetime
+import numpy as np
+import tensorflow as tf
+import CNNModels_joe
+
+class vgg_seg1:
+    def __init__(self, x, y, seg_labels, keep_dropout, train_phase):
+        self.prob_class, self.logits_seg = CNNModels_joe.VGG_Seg1(x, keep_dropout, train_phase, debug=True)
+        self.loss_seg = loss_seg_norm(seg_labels, self.logits_seg)
+        self.loss_class = loss_class(y, self.prob_class)
+
+def loss_seg_norm(y, logits):
+    newy = y/tf.tile(tf.expand_dims(tf.reduce_sum(y,axis=-1), axis=-1), (1,1,1,176))
+    return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=newy, logits=logits))
+
+def loss_class(y, prob):
+    newy = tf.one_hot(y, 100)
+    return -tf.reduce_mean(tf.reduce_sum(newy * tf.log(prob + 1e-8), axis=-1))
+
+#def loss_class(y, prob):
+#    return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=prob))
+
---
+++ exp_seg.py
@@ -0,0 +1,19 @@
+exp_seg={
+	'exp1':{
+		'learning_rate_class' : 1e-5,
+		'learning_rate_seg' : 1e-5,
+		'training_iters' : 10000,
+		'step_display' : 50,
+		'step_save' : 500,
+		'exp_name' : 'exp1',
+		'pretrainedStep' : 0,
+		'batch_size':15,
+		'joint_ratio':1.0,
+		'plot':True,
+
+		'train' : True,
+		'validation' : True,
+		'test': False,
+		'selectedmodel':"vgg_seg1"
+	},
+}
