205ddd19a5b8f6c22ed1caa8749edcc4f0cdd8e8
joehuang <joehuang@mit.edu>
2017-12-11 22:44:54 -0500

    update

--- exp_joe.py
+++ exp_joe.py
@@ -3,7 +3,7 @@ exp_joe={
 		'learning_rate_class' : 1e-5,
 		'learning_rate_seg' : 1e-5,
 		'training_iters' : 10000,
-		'step_display' : 50,
+		'step_display' : 20,
 		'step_save' : 500,
 		'exp_name' : 'exp1',
 		'pretrainedStep' : 0,
